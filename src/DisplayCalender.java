d8ec43cfb87cddf0b7b3f651863f59934c64b609
Rediet Ayalew <rediet2000@gmail.com>
2017-06-01 00:25:23 -0400

    "Calendar with leap year"

--- src/DisplayCalender.java
+++ src/DisplayCalender.java
@@ -2,79 +2,72 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Scanner;
 
 public class DisplayCalender {
 
 	public static void main(String[] args) {
 
-//		System.out.println("Enter the month");
-//		System.out.println("Enter the year");
+		 Scanner scn = new Scanner(System.in);
+		 
+		 System.out.println("Enter the year");
+		 int year = scn.nextInt();
+		 
+		 System.out.println("Enter the month");
+		 int month = scn.nextInt();
 		
-		PrintCalender();
-		
+		 System.out.println("Enter the date");
+		 int date = scn.nextInt();
+
+		PrintCalender(year, month, date);
+
 	}
+
+	public static void PrintCalender(int year, int month, int date) {
+
+		String months[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct","Nov", "Dec" };
+		String daysInWeek[] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+		GregorianCalendar gc = new GregorianCalendar();
+		
+		gc.set(year, month, date);
 	
-	public static void PrintCalender(){
-		
-		String months[] = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Dec"};
-		String daysInWeek[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
-		int date[] = new int[31];
-		GregorianCalendar gc = new GregorianCalendar(2017,0,30);
-		
-		
+
 		System.out.print(months[gc.get(Calendar.MONTH)]);
 		System.out.println(" " + gc.get(Calendar.YEAR));
-		
-		for(int i=0; i< daysInWeek.length; i++){
-			
-			System.out.print(daysInWeek[i]+ "\t");
-		}
-		
-		for(int j=1; j< date.length; j++){
-			
-			date[j] = j;
-		}
-		
-		
 
-		int dayOfWeek = gc.get(Calendar.DAY_OF_WEEK)-1;
-		
+		for (int i = 0; i < daysInWeek.length; i++) {
+
+			System.out.print(daysInWeek[i] + "\t");
+		}
 		System.out.println();
-		int	counter = 1;
-		while(counter!=dayOfWeek){
-			System.out.print("\t");
-			counter++;
-		}
-		
-		
-		for(int i = 1; i < date.length; i++){
-			
-			
-			switch(counter){
-			
-			case 1: System.out.print(date[i]+ "\t");
-					break;
-			case 2: System.out.print(date[i]+ "\t");
-					break;
-			case 3: System.out.print(date[i]+ "\t");
-					break;
-			case 4: System.out.print(date[i]+ "\t");
-					break;
-			case 5: System.out.print(date[i]+ "\t");
-					break;
-			case 6: System.out.print(date[i]+ "\t");
-					break;
-			case 7: System.out.println(date[i]);
-					break;
-			}
-		
-			if (counter == 7){
-				counter=1;
+
+		int firstDayOfWeek = gc.get(Calendar.DAY_OF_WEEK) - 1;
+		int daysInMonth = gc.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+		int dayOfTheWeek = 0;
+		int counter = 1;
+		boolean firstDayPrinted = false;
+
+		while (counter <= daysInMonth) {
+			if (!firstDayPrinted) {
+				if (dayOfTheWeek != firstDayOfWeek) {
+					System.out.print("\t");
+				} else {
+					System.out.print(counter++ + "\t");
+					firstDayPrinted = true;
+				}
 			} else {
-				counter++;
+				System.out.print(counter++ + "\t");
 			}
-			
+
+			if (dayOfTheWeek == 6) {
+				System.out.println();
+				dayOfTheWeek = 0;
+			} else {
+				dayOfTheWeek++;
+			}
+
 		}
-	
+
 	}
 }
